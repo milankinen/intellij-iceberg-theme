2062ce1f2af076c38a81dd2035c9f2b60a91ae70
GitHub Action <action@github.com>
2020-09-19 16:25:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/milankinen/ideaicebergtheme/MyBundle.kt
+++ src/main/kotlin/com/github/milankinen/ideaicebergtheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.milankinen.ideaicebergtheme
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/milankinen/ideaicebergtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.milankinen.ideaicebergtheme.services
+
+import com.github.milankinen.ideaicebergtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
